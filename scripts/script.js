66a93823df3dcaecd4d89b6b38c215466c4f4526
Felipe Jaramillo <Artimax85@gmail.com>
2022-06-28 20:28:54 -0500

    Recorrer array

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let nombrePlanetas = Array('Tierra', 'Marte', 'Jupiter');
 let distancia =[6,3,'67'];
-const tamaño = [];
+const tamaño = [10,20,30];
 
 console.log(nombrePlanetas);
 console.log(distancia);
@@ -17,3 +17,17 @@ for(const key in distancia){
         console.log("No se encontro la distancia");
     }
 }
+nombrePlanetas.forEach((value,index,array) =>{
+    console.log('planeta',index,value);
+});
+
+nombrePlanetas.map((planetas,index)=>{
+    return(
+        console.log(`El planeta ${planetas} tiene tamaño de : ${tamaño[index]}`)
+    )
+});
+let resultadoMap = nombrePlanetas.map((planetas,index)=>{
+    return(`El planeta ${planetas} tiene tamaño de :${tamaño[index]}`)
+});
+
+console.log(resultadoMap);
